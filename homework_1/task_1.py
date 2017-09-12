356d948252465161615dfa163f4c716d0c8883cf
phuuda <phuuda@gmail.com>
2017-09-12 11:32:21 +0300

    code edit

--- homework_1/task_1.py
+++ homework_1/task_1.py
@@ -5,7 +5,10 @@ def fibonacci(n):
         if n == 1 or n == 0:
             return n
 
-        if n > 1:
+        if n == 2:
+            return 1
+
+        if n > 2:
     
             n1, n2 = 1, 1
             n3 = n1 + n2
@@ -23,6 +26,10 @@ def fibonacci(n):
         
 print(fibonacci(20.4))
 print(fibonacci('door'))
+print(fibonacci(-5))
 print(fibonacci(20))
 print(fibonacci(0))
 print(fibonacci(1))
+print(fibonacci(2))
+print(fibonacci(3))
+print(fibonacci(4))
